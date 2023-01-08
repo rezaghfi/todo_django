ea6746f633ffc92ca574cb4d80184fc0701c17f5
reza gholamalitabar <reza.gh.fi@gmail.com>
2023-01-09 03:18:08 +0330

    jalase home and detail page in todo site

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
   path('hello/', views.say_hello),
-  path('', views.home)
+  path('', views.home),
+  path('detail/<int:todo_id>/', views.detail)
 ]
--- home/views.py
+++ home/views.py
@@ -6,9 +6,15 @@ from .models import Todo
 
 def say_hello(request):
   # return HttpResponse("hello")
-  person = {'name':'reza', 'age':29}
-  return render(request, 'hello.html', context=person)
+  reza = {'name':'reza', 'age':29, 'range':range(9)}
+  admin = {'name':'admin', 'age':28, 'range':range(10)}
+  user = {'name':'user', 'age': 3}
+  return render(request, 'hello.html', context=admin)
 
 def home(request):
   all = Todo.objects.all()
   return render(request, 'home.html', {'todos': all})
+
+def detail(request, todo_id):
+  todo = Todo.objects.get(id= todo_id)
+  return render(request, 'detail.html', {'todo':todo})
