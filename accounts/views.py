36e2a28376d26c23f1e42d7079574a0004d04d50
Reza Gholamalitabar <reza.gh.fi@gmail.com>
2023-02-05 15:04:38 +0330

    jalase 11

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from accounts.forms import UserRegisterForm
 
-def home(request):
-  return render(request, 'home.html')
+
+def user_register(request):
+  if request.method == "POST":
+    form = UserRegisterForm(request.POST)
+    if form.is_valid():
+      cd = form.cleaned_data
+      User.objects.create_user(username=cd['username'], email=cd['email'], password=cd['password'])
+      messages.success(request, 'کاربر با موفقیت ایجاد شد')
+      return redirect('home')
+  else:
+    form = UserRegisterForm()
+  return render(request, 'register.html', {'form':form})
